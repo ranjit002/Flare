2639e8b5b5581a56ce2aa58af69958c8f78c13ca
ranjit002 <rs2190@cam.ac.uk>
2025-09-07 23:56:24 +0100

    Moved Ops parameter choices to Solver

--- include/Flare/solver/BasicSolver.h
+++ include/Flare/solver/BasicSolver.h
@@ -10,6 +10,11 @@ class BasicSolver : public ISolver
 {
  public:
   void step(fluid::IFluid& fluid, float dt) override;
+  
+  float visc = 0.1f;
+  float diff = 0.001f;
+  int diffuse_iter = 10;
+  int project_iter = 10;
 };
 
 }  // namespace solver
--- include/Flare/solver/Ops.h
+++ include/Flare/solver/Ops.h
@@ -8,12 +8,9 @@ namespace solver::ops
 
 void addForces(fluid::IFluid& fluid, const Eigen::Vector3f& f, float dt);
 void advect(fluid::IFluid& fluid, float dt);
-void diffuse(fluid::IFluid& fluid, float dt, float visc = 0.1f, int iter = 10);
-void project(fluid::IFluid& fluid, int iter = 20);
+void diffuse(fluid::IFluid& fluid, float dt, float visc, int iter);
+void project(fluid::IFluid& fluid, int iter);
 void advectDensity(fluid::IFluid& fluid, float dt);
-void diffuseDensity(fluid::IFluid& fluid,
-    float dt,
-    float diff = 0.001f,
-    int iter = 10);
+void diffuseDensity(fluid::IFluid& fluid, float dt, float diff, int iter);
 
 }  // namespace solver::ops
