34b6c2a5e41bc1c3b1bc2895e3bcf980380f0118
ranjit002 <rs2190@cam.ac.uk>
2025-09-08 17:21:56 +0100

    Added documentation to ISolver.h

--- include/Flare/solver/ISolver.h
+++ include/Flare/solver/ISolver.h
@@ -5,16 +5,60 @@
 namespace solver
 {
 
+/**
+ * @brief Abstract interface for fluid solvers.
+ *
+ * Solver operates on a `fluid::Fluid` object, advancing its state forward in
+ * time.
+ *
+ * ### Responsibilities:
+ * - Implementing the core simulation step through step().
+ * - Managing boundary conditions (BCs) (e.g., walls, inflows, obstacles).
+ * - Updates the velocity, density, and other scalar fields in the simulation.
+ *
+ * @note
+ * - This class manages a list of BC objects, each of which
+ *   defines how the fluid interacts with walls, obstacles, or inflows.
+ * - Boundary objects are stored using std::unique_ptr, ensuring that
+ *   `ISolver` owns their lifetime.
+ */
 class ISolver
 {
    public:
     virtual ~ISolver() = default;
+
+    /**
+     * @brief Advances the simulation by a single timestep.
+     *
+     * @param fluid Fluid container object (see Flare/fluid/Fluid.h)
+     * @param dt The timestep size in seconds.
+     */
     virtual void step(fluid::Fluid& fluid, float dt) = 0;
+
+    /**
+     * @brief Adds new BC object to solver.
+     *
+     * Function transfers ownership of BC object
+     * to the solver. Solver will apply all registered BCs
+     * during each call to step().
+     *
+     * @param bc Unique pointer to BC object (IBoundary)
+     *
+     * @note
+     * - Ownership is transferred, DON'G use the pointer after
+     * passing it!
+     * - Can add multiple BCs objectt.
+     */
     void addBC(std::unique_ptr<boundary::IBoundary> bc)
     {
         bcs_.push_back(std::move(bc));
     }
 
+    /**
+     * @brief Read-only access to registered BCs.
+     *
+     * @return Const reference to vector of BCs
+     */
     const std::vector<std::unique_ptr<boundary::IBoundary>>& boundaries() const
     {
         return bcs_;
