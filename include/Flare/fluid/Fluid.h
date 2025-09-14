bbac14c47b05b252bb9f22d100dfd9321b6b2009
ranjit002 <rs2190@cam.ac.uk>
2025-09-14 07:55:06 +0100

    Moved implementation of Fluid class to Fluid.cpp

--- include/Flare/fluid/Fluid.h
+++ include/Flare/fluid/Fluid.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <Eigen/Dense>
-#include <iostream>
 
 #include "Field.h"
 
@@ -31,57 +30,32 @@ class Fluid
         int height,
         int depth,
         float default_density = 0.0f,
-        Eigen::Vector3f default_velocity = Eigen::Vector3f::Zero())
-        : width_(width),
-          height_(height),
-          depth_(depth),
-          density_(width, height, depth, default_density),
-          velocity_(width, height, depth, default_velocity)
-    {
-    }
+        Eigen::Vector3f default_velocity = Eigen::Vector3f::Zero());
 
     [[nodiscard]] int width() const noexcept { return width_; }
     [[nodiscard]] int height() const noexcept { return height_; }
     [[nodiscard]] int depth() const noexcept { return depth_; }
-    [[nodiscard]] size_t size() const noexcept { return density_.size(); }
+    [[nodiscard]] size_t size() const noexcept;
 
-    [[nodiscard]] float getDensity(int x, int y, int z) const
-    {
-        return density_(x, y, z);
-    }
-    void setDensity(int x, int y, int z, float value)
-    {
-        density_.set(x, y, z, value);
-    }
+    [[nodiscard]] float getDensity(int x, int y, int z) const;
+    void setDensity(int x, int y, int z, float value);
     [[nodiscard]] const FieldFloat& density() const noexcept
     {
         return density_;
     }
     [[nodiscard]] FieldFloat& density() noexcept { return density_; }
 
-    [[nodiscard]] const Eigen::Vector3f& getVelocity(int x, int y, int z) const
-    {
-        return velocity_(x, y, z);
-    }
-    void setVelocity(int x, int y, int z, const Eigen::Vector3f& vel)
-    {
-        velocity_.set(x, y, z, vel);
-    }
+    [[nodiscard]] const Eigen::Vector3f& getVelocity(int x, int y, int z) const;
+    void setVelocity(int x, int y, int z, const Eigen::Vector3f& vel);
     [[nodiscard]] const FieldVector& velocity() const noexcept
     {
         return velocity_;
     }
     [[nodiscard]] FieldVector& velocity() noexcept { return velocity_; }
 
-    void clearDensity(float value = 0.0f) { density_.fill(value); }
-    void clearVelocity(const Eigen::Vector3f& value = Eigen::Vector3f::Zero())
-    {
-        velocity_.fill(value);
-    }
-    void printDensity()
-    {
-        std::cout << density_.get().transpose() << std::endl;
-    }
+    void clearDensity(float value = 0.0f);
+    void clearVelocity(const Eigen::Vector3f& value = Eigen::Vector3f::Zero());
+    void printDensity();
 
    private:
     int width_, height_, depth_;
---
+++ src/fluid/Fluid.cpp
@@ -0,0 +1,52 @@
+#include "Flare/fluid/Fluid.h"
+
+#include <iostream>
+
+namespace fluid
+{
+
+Fluid::Fluid(int width,
+    int height,
+    int depth,
+    float default_density,
+    Eigen::Vector3f default_velocity)
+    : width_(width),
+      height_(height),
+      depth_(depth),
+      density_(width, height, depth, default_density),
+      velocity_(width, height, depth, default_velocity)
+{
+}
+
+size_t Fluid::size() const noexcept { return density_.size(); }
+
+float Fluid::getDensity(int x, int y, int z) const { return density_(x, y, z); }
+
+void Fluid::setDensity(int x, int y, int z, float value)
+{
+    density_.set(x, y, z, value);
+}
+
+const Eigen::Vector3f& Fluid::getVelocity(int x, int y, int z) const
+{
+    return velocity_(x, y, z);
+}
+
+void Fluid::setVelocity(int x, int y, int z, const Eigen::Vector3f& vel)
+{
+    velocity_.set(x, y, z, vel);
+}
+
+void Fluid::clearDensity(float value) { density_.fill(value); }
+
+void Fluid::clearVelocity(const Eigen::Vector3f& value)
+{
+    velocity_.fill(value);
+}
+
+void Fluid::printDensity()
+{
+    std::cout << density_.get().transpose() << std::endl;
+}
+
+}  // namespace fluid
