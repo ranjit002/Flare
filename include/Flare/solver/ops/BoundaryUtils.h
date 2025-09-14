3735a02878a1162592837f85ea361eb9dabc9738
ranjit002 <rs2190@cam.ac.uk>
2025-09-14 12:01:21 +0100

    Removed BoundaryUtils.cpp file

--- include/Flare/solver/ops/BoundaryUtils.h
+++ include/Flare/solver/ops/BoundaryUtils.h
@@ -8,21 +8,39 @@
 namespace solver::ops
 {
 
-bool isSolidCell(const std::vector<std::unique_ptr<boundary::IBoundary>>& bcs,
+inline bool isSolidCell(const std ::vector<std::unique_ptr<boundary::IBoundary>>& bcs,
     int x,
     int y,
-    int z);
+    int z)
+{
+    for (auto& bc : bcs)
+        if (bc->isSolid(x, y, z))
+            return true;
+    return false;
+}
 
-Eigen::Vector3f getWallVelocity(
+inline Eigen::Vector3f getWallVelocity(
     const std::vector<std::unique_ptr<boundary::IBoundary>>& bcs,
     int x,
     int y,
-    int z);
+    int z)
+{
+    for (auto& bc : bcs)
+        if (bc->isSolid(x, y, z))
+            return bc->wallVelocity(x, y, z);
+    return Eigen::Vector3f::Zero();
+}
 
-float getWallDensity(
+inline float getWallDensity(
     const std::vector<std::unique_ptr<boundary::IBoundary>>& bcs,
     int x,
     int y,
-    int z);
+    int z)
+{
+    for (auto& bc : bcs)
+        if (bc->isSolid(x, y, z))
+            return bc->wallDensity(x, y, z);
+    return 0.f;
+}
 
 }  // namespace solver::ops
