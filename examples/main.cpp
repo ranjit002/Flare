63ca0411d456f5b7ec6a1b4edf8d0b93f861ef78
ranjit002 <rs2190@cam.ac.uk>
2025-09-14 08:57:37 +0100

    Appeasing clangd

--- examples/main.cpp
+++ examples/main.cpp
@@ -35,8 +35,8 @@ int main()
     solver.addBCs(boundary::Inflow(Eigen::Vector3f(maxSpeed, 0, 0), 100),
         boundary::Circle(Eigen::Vector3f(W / 2.f, H / 2.f, D / 2.f), 10),
         boundary::Box(W, H, D));
-    
-        // SFML window
+
+    // SFML window
     sf::RenderWindow window(sf::VideoMode({SCALE_FACTOR * W, SCALE_FACTOR * H}),
         "Fluid Simulation");
     window.setFramerateLimit(60);
